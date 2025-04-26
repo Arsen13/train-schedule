f599733a7a4fe47b86eaaf9f814bf3aa3937e98c
Arsen Kohut <arsenkogut2003@gmail.com>
2025-04-26 15:22:14 +0300

    Create buttons for pagination

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar/Navbar";
+import PaginationButtons from "@/components/records/PaginationBtn";
 import Records from "@/components/records/Records";
 import SearchField from "@/components/searchField/SearchField";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="max-w-3xl flex items-center gap-6 flex-col mt-20  mx-auto">
         <SearchField />
         <Records />
+        <PaginationButtons />
       </div>
     </>
   );
---
+++ frontend/components/records/PaginationBtn.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export default function PaginationButtons() {
+  return (
+    <div className="flex justify-center gap-4">
+      <button
+        onClick={() => {}}
+        className="cursor-pointer rounded-md px-2 py-1 text-sm duration-500 hover:bg-amber-600"
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => {}}
+        className="cursor-pointer rounded-md px-2 py-1 text-sm duration-500 hover:bg-amber-600"
+      >
+        Next
+      </button>
+    </div>
+  );
+}

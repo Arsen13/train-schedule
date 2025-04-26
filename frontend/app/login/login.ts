c728e33dafea97c5c6b97ca5c0d7718ea92b2194
Arsen Kohut <arsenkogut2003@gmail.com>
2025-04-26 22:32:55 +0300

    Add sameSite attribute to cookie

--- frontend/app/login/login.ts
+++ frontend/app/login/login.ts
@@ -2,6 +2,7 @@
 
 import { post } from "@/lib/fetch";
 import { jwtDecode } from "jwt-decode";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { cookies } from "next/headers";
 
 export default async function login(formData: FormData) {
@@ -13,13 +14,15 @@ export default async function login(formData: FormData) {
   const setCookieHeader = errorOrRes?.headers.get("Set-Cookie");
   if (setCookieHeader) {
     const token = setCookieHeader.split(";")[0].split("=")[1];
-    (await cookies()).set({
+    const cookie: ResponseCookie = {
       name: "Authentication",
       value: token,
       secure: true,
       httpOnly: true,
       expires: new Date(jwtDecode(token).exp! * 1000),
-    });
+      sameSite: "none",
+    };
+    (await cookies()).set(cookie);
   }
 
   return errorOrRes?.body;
